d90618243d36279f66676fa738f0f6b9857cd753
MishaS2002 <71435951+MishaS2002@users.noreply.github.com>
2020-11-24 21:23:45 +0200

    Add files via upload

---
+++ Клас 24,11,2020.py
@@ -0,0 +1,22 @@
+
+
+
+import math
+class SKIF():
+  def GPA(self, name, e1, e2, e3) :
+   self. name = name
+    self.e1 = e1
+    self.e2 = e2
+    self.e3 = e3
+     print(self. name, ' - ',((self.e1 + self.e2 + self.e3)/3))
+
+     s1 = Student()
+     s2 = Student()
+     s1.GPA('Dmytro', 5, 3, 4)
+     s2.GPA('Olena', 5, 4, 5)
+     
+     def main():
+          return 0
+
+     if __name__ == '__main__':
+          main()
